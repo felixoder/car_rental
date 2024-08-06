c8da0d1943be056068b2eb0778bb83f34bb42f19
felixoder <debayanghosh408@gmail.com>
2024-08-06 23:49:13 +0530

    q

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import Hero from '@/components/Hero'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import "@/app/globals.css"
 import BoxContent from '@/components/BoxContent'
 import AboutSection from '@/components/AboutSection'
@@ -8,10 +8,26 @@ import Places from '@/components/Places'
 import CarSections from '@/components/CarSections'
 import ServiceSections from '@/components/ServiceSections'
 import FeedbackSection from '@/components/FeedbackSection'
+import useIsClient from '@/components/useIsClient'
 
 type Props = {}
 
 const page = (props: Props) => {
+  const [windowWidth, setWindowWidth] = useState<number>(0);
+  const isClient = useIsClient();
+
+  useEffect(() => {
+    if (isClient) {
+      const handleResize = () => setWindowWidth(window.innerWidth);
+      handleResize(); // Initialize width on mount
+
+      window.addEventListener("resize", handleResize);
+      return () => window.removeEventListener("resize", handleResize);
+    }
+    else{
+      <p>loading...</p>
+    }
+  }, [isClient]);
   return (
     <div>
       <Hero/>
